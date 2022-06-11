5fedeb27272ae77752ab0fa92a488fa07fb5730d
“jovannimetal” <jovanni666@hotmail.com>
2022-06-10 20:40:10 -0500

    Ejercicios modulo 2

---
+++ basico/modulo2/lab2512.py
@@ -0,0 +1,12 @@
+# este programa calcula los segundos en cierto número de horas determinadas 
+
+hora = 2 # número de horas
+seconds = 3600 # número de segundos en una hora
+
+print("Horas: ", hora) #imprime el numero de horas
+print("Segundos en Horas: ", hora * seconds) # se imprime el numero de segundos en determinado numero de horas
+
+'''aquí también se debe de imprimir un "Adiós", 
+pero el programador no tuvo tiempo de escribirlo
+este el es fin del programa que calcula el numero de segundos en 2 horas
+'''
