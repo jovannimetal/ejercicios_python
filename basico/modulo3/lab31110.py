c25996c6f0115e76fe5eaa6774932b621a311579
“jovannimetal” <jovanni666@hotmail.com>
2022-06-14 16:39:10 -0500

    Ejercicios modulo 3

---
+++ basico/modulo3/lab31110.py
@@ -0,0 +1,8 @@
+word = input('Ingresas el nombre de planta')
+
+if word == "ESPATIFILIO":
+    print("Si, ¡El ESPATIFILIO! es la mejor planta de todos los tiempos!" )
+elif word == "espatifilo":
+    print("No, ¡quiero un gran ESPATIFILIO!")
+else:
+    print ("!ESPATIFILIO!, ¡No pelargonio!")
---
+++ basico/modulo3/lab3114.py
@@ -0,0 +1,5 @@
+num = input ('Ingresa un número: ')
+num = int (num)
+
+print (num < 100)
+print (num >= 100)
